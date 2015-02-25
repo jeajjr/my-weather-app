560fa2309ebe3d016b3a7cc1982a0a4eae8b4bc6
jeajjr <jeajjr@gmail.com>
2015-02-25 02:33:15 -0500

    Created Android project.

---
+++ WeatherInternshipProject/app/src/main/java/com/homespotter/weatherinternshipproject/MainActivity.java
@@ -0,0 +1,18 @@
+package com.homespotter.weatherinternshipproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Weather app template project.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //TODO Initialize activity state and variables if necessary
+    }
+}
