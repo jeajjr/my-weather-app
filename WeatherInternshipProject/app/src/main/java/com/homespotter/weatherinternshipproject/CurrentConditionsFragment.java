6efede3d14e01a025b66096804bbfdf1dc60c3f2
jeajjr <jeajjr@gmail.com>
2015-03-01 22:18:11 -0500

    Updated current conditions layout.

--- WeatherInternshipProject/app/src/main/java/com/homespotter/weatherinternshipproject/CurrentConditionsFragment.java
+++ WeatherInternshipProject/app/src/main/java/com/homespotter/weatherinternshipproject/CurrentConditionsFragment.java
@@ -15,7 +15,6 @@ public class CurrentConditionsFragment extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
