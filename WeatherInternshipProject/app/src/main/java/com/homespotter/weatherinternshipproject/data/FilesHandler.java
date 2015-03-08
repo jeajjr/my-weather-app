39a4087fcce25e795f5346ec739dd765e08abf6c
jeajjr <jeajjr@gmail.com>
2015-03-07 19:34:56 -0500

    Activity Main now uses DialogFragment.

--- WeatherInternshipProject/app/src/main/java/com/homespotter/weatherinternshipproject/data/FilesHandler.java
+++ WeatherInternshipProject/app/src/main/java/com/homespotter/weatherinternshipproject/data/FilesHandler.java
@@ -105,6 +105,10 @@ public class FilesHandler {
         Log.d(TAG, "addCityName");
 
         ArrayList<String> cityList = getSavedCities(context);
+
+        if (cityList == null)
+            cityList = new ArrayList<String>();
+
         cityList.add(cityName);
         setCityList(context, cityList);
 
