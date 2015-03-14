6f3587e92fe2f07c8a455e73378314673a1175e6
jeajjr <jeajjr@gmail.com>
2015-03-14 12:34:28 -0400

    Added dummy server connection for testing.
    The OpenWeatherWeb are not stably working.

--- WeatherInternshipProject/app/src/main/java/com/homespotter/weatherinternshipproject/ui/WeatherAppWidgetProvider.java
+++ WeatherInternshipProject/app/src/main/java/com/homespotter/weatherinternshipproject/ui/WeatherAppWidgetProvider.java
@@ -37,6 +37,8 @@ public class WeatherAppWidgetProvider extends AppWidgetProvider{
                     // TODO remove
                     String data = //"{\"coord\":{\"lon\":-0.13,\"lat\":51.51},\"sys\":{\"message\":0.0774,\"country\":\"GB\",\"sunrise\":1426227494,\"sunset\":1426269695},\"weather\":[{\"id\":802,\"main\":\"Clouds\",\"description\":\"scattered clouds\",\"icon\":\"03n\"}],\"base\":\"cmc stations\",\"main\":{\"temp\":278.742,\"temp_min\":278.742,\"temp_max\":278.742,\"pressure\":1029.22,\"sea_level\":1039.72,\"grnd_level\":1029.22,\"humidity\":71},\"wind\":{\"speed\":4.12,\"deg\":54.501},\"clouds\":{\"all\":32},\"dt\":1426282599,\"id\":2643743,\"name\":\"London\",\"cod\":200}";
 
+                    //try { Thread.sleep(1500 + (long) (Math.random() * 1000.0)); } catch (Exception e) {}
+
                     WeatherClient.getInstance().getCurrentConditionsData(cityName, SettingsProfile.UNIT_METRIC);
                     CurrentConditions currentConditions = DataParser.parseCurrentConditions(data);
 
