8fec722e6835d74dba38c428f3907ca4c4b5099f
jeajjr <jeajjr@gmail.com>
2015-03-14 12:41:11 -0400

    Added dummy server connection for testing.

--- WeatherInternshipProject/app/src/main/java/com/homespotter/weatherinternshipproject/ui/ActivityMain.java
+++ WeatherInternshipProject/app/src/main/java/com/homespotter/weatherinternshipproject/ui/ActivityMain.java
@@ -40,7 +40,7 @@ public class ActivityMain extends ActionBarActivity implements DataProviderInter
     private final static String TAG = "ActivityMain";
 
     //TODO remove
-    boolean debugConnection = false;
+    public static boolean debugConnection = true;
 
     // Weather related data
     private CurrentConditions currentConditions = null;
@@ -163,8 +163,6 @@ public class ActivityMain extends ActionBarActivity implements DataProviderInter
                     currentConditionsDataReady = false;
                     Log.d(TAG, "Getting current conditions");
 
-                    //try { Thread.sleep(5000); } catch (Exception e) { Log.d(TAG, "error sleep "); }
-
                     Log.d(TAG, "fetchCurrentConditions with " + settingsProfile + " for " + cityName);
 
                     //TODO remove
@@ -239,7 +237,13 @@ public class ActivityMain extends ActionBarActivity implements DataProviderInter
                 // TODO remove
 
                 if (debugConnection) {
+                    String data = "{\"cod\":\"200\",\"message\":9.4357,\"city\":{\"id\":2643743,\"name\":\"London\",\"coord\":{\"lon\":-0.12574,\"lat\":51.50853},\"country\":\"GB\",\"population\":0,\"sys\":{\"population\":0}},\"cnt\":2,\n" +
+                            "\"list\":[{\"dt\":1426269600,\"main\":{\"temp\":278.74,\"temp_min\":278.74,\"temp_max\":281.083,\"pressure\":1027.39,\"sea_level\":1037.72,\"grnd_level\":1027.39,\"humidity\":61,\"temp_kf\":-2.34},\"weather\":[{\"id\":803,\"main\":\"Clouds\",\"description\":\"broken clouds\",\"icon\":\"04d\"}],\"clouds\":{\"all\":56},\"wind\":{\"speed\":3.92,\"deg\":56.0004},\"rain\":{\"3h\":0},\"sys\":{\"pod\":\"d\"},\"dt_txt\":\"2015-03-13 18:00:00\"},{\"dt\":1426280400,\"main\":{\"temp\":276.52,\"temp_min\":276.52,\"temp_max\":278.742,\"pressure\":1029.22,\"sea_level\":1039.72,\"grnd_level\":1029.22,\"humidity\":71,\"temp_kf\":-2.22},\"weather\":[{\"id\":802,\"main\":\"Clouds\",\"description\":\"scattered clouds\",\"icon\":\"03n\"}],\"clouds\":{\"all\":32},\"wind\":{\"speed\":4.12,\"deg\":54.501},\"rain\":{\"3h\":0},\"sys\":{\"pod\":\"n\"},\"dt_txt\":\"2015-03-13 21:00:00\"}]}";
+                    threeHoursForecast = DataParser.parseFiveDaysForecast(data);
 
+                    try { Thread.sleep(1500 + (long) (Math.random() * 1000.0)); } catch (Exception e) {}
+                }
+                else {
                     try {
                         String data = WeatherClient.getInstance().getThreeHoursForecastData(cityName, settingsProfile.getUnits());
                         threeHoursForecast = DataParser.parseFiveDaysForecast(data);
@@ -251,13 +255,6 @@ public class ActivityMain extends ActionBarActivity implements DataProviderInter
                             }
                         });
                     }
-                }
-                else {
-                    String data = "{\"cod\":\"200\",\"message\":9.4357,\"city\":{\"id\":2643743,\"name\":\"London\",\"coord\":{\"lon\":-0.12574,\"lat\":51.50853},\"country\":\"GB\",\"population\":0,\"sys\":{\"population\":0}},\"cnt\":2,\n" +
-                            "\"list\":[{\"dt\":1426269600,\"main\":{\"temp\":278.74,\"temp_min\":278.74,\"temp_max\":281.083,\"pressure\":1027.39,\"sea_level\":1037.72,\"grnd_level\":1027.39,\"humidity\":61,\"temp_kf\":-2.34},\"weather\":[{\"id\":803,\"main\":\"Clouds\",\"description\":\"broken clouds\",\"icon\":\"04d\"}],\"clouds\":{\"all\":56},\"wind\":{\"speed\":3.92,\"deg\":56.0004},\"rain\":{\"3h\":0},\"sys\":{\"pod\":\"d\"},\"dt_txt\":\"2015-03-13 18:00:00\"},{\"dt\":1426280400,\"main\":{\"temp\":276.52,\"temp_min\":276.52,\"temp_max\":278.742,\"pressure\":1029.22,\"sea_level\":1039.72,\"grnd_level\":1029.22,\"humidity\":71,\"temp_kf\":-2.22},\"weather\":[{\"id\":802,\"main\":\"Clouds\",\"description\":\"scattered clouds\",\"icon\":\"03n\"}],\"clouds\":{\"all\":32},\"wind\":{\"speed\":4.12,\"deg\":54.501},\"rain\":{\"3h\":0},\"sys\":{\"pod\":\"n\"},\"dt_txt\":\"2015-03-13 21:00:00\"}]}";
-                    threeHoursForecast = DataParser.parseFiveDaysForecast(data);
-
-                    try { Thread.sleep(1500 + (long) (Math.random() * 1000.0)); } catch (Exception e) {}
                 }
 
                 runOnUiThread(new Runnable() {
@@ -304,7 +301,7 @@ public class ActivityMain extends ActionBarActivity implements DataProviderInter
 
                     // TODO remove
                     if (debugConnection) {
-                        try { Thread.sleep(5000); } catch (Exception e) { Log.d(TAG, "error sleep "); }
+                        try { Thread.sleep(1500 + (long) (Math.random() * 1000.0)); } catch (Exception e) {}
 
                         String data = "{\"cod\":\"200\",\"message\":0.6164,\"city\":{\"id\":2643743,\"name\":\"London\",\"coord\":{\"lon\":-0.12574,\"lat\":51.50853},\"country\":\"GB\",\"population\":0,\"sys\":{\"population\":0}},\"cnt\":7,\"list\":[{\"dt\":1426248000,\"temp\":{\"day\":280.85,\"min\":276.51,\"max\":280.85,\"night\":276.51,\"eve\":280.85,\"morn\":280.85},\"pressure\":1031.66,\"humidity\":0,\"weather\":[{\"id\":500,\"main\":\"Rain\",\"description\":\"light rain\",\"icon\":\"10d\"}],\"speed\":4.77,\"deg\":59,\"clouds\":29,\"rain\":1.65},{\"dt\":1426334400,\"temp\":{\"day\":279.7,\"min\":276.46,\"max\":279.92,\"night\":278.16,\"eve\":278.77,\"morn\":276.46},\"pressure\":1037.73,\"humidity\":83,\"weather\":[{\"id\":500,\"main\":\"Rain\",\"description\":\"light rain\",\"icon\":\"10d\"}],\"speed\":5.62,\"deg\":54,\"clouds\":64,\"rain\":0.41},{\"dt\":1426420800,\"temp\":{\"day\":279.76,\"min\":276.62,\"max\":279.89,\"night\":276.62,\"eve\":278.64,\"morn\":277.94},\"pressure\":1035.61,\"humidity\":86,\"weather\":[{\"id\":500,\"main\":\"Rain\",\"description\":\"light rain\",\"icon\":\"10d\"}],\"speed\":6.41,\"deg\":52,\"clouds\":88,\"rain\":0.45},{\"dt\":1426507200,\"temp\":{\"day\":280.51,\"min\":277.43,\"max\":280.82,\"night\":277.43,\"eve\":280.32,\"morn\":277.56},\"pressure\":1030.74,\"humidity\":91,\"weather\":[{\"id\":500,\"main\":\"Rain\",\"description\":\"light rain\",\"icon\":\"10d\"}],\"speed\":3.57,\"deg\":75,\"clouds\":88,\"rain\":0.62},{\"dt\":1426593600,\"temp\":{\"day\":280.69,\"min\":276.12,\"max\":281.54,\"night\":276.93,\"eve\":280.46,\"morn\":276.12},\"pressure\":1036.07,\"humidity\":94,\"weather\":[{\"id\":500,\"main\":\"Rain\",\"description\":\"light rain\",\"icon\":\"10d\"}],\"speed\":2.06,\"deg\":335,\"clouds\":92},{\"dt\":1426680000,\"temp\":{\"day\":282.54,\"min\":272.98,\"max\":282.54,\"night\":275.73,\"eve\":281.42,\"morn\":272.98},\"pressure\":1042.47,\"humidity\":0,\"weather\":[{\"id\":500,\"main\":\"Rain\",\"description\":\"light rain\",\"icon\":\"10d\"}],\"speed\":2.56,\"deg\":75,\"clouds\":10},{\"dt\":1426766400,\"temp\":{\"day\":281.47,\"min\":274.95,\"max\":281.47,\"night\":277.85,\"eve\":281.25,\"morn\":274.95},\"pressure\":1030.88,\"humidity\":0,\"weather\":[{\"id\":500,\"main\":\"Rain\",\"description\":\"light rain\",\"icon\":\"10d\"}],\"speed\":2.75,\"deg\":60,\"clouds\":79,\"rain\":0.62}]}";
                         dailyForecast = DataParser.parseDailyForecast(data);
